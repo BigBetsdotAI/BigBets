222c7689131e94b8f27a61b5066af6d0f12819de
Shishir Umrao <shishirumrao42@gmail.com>
2025-08-01 11:18:55 +0530

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,24 @@ import tagger from "@dhiwise/component-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "build",
-  },
-  plugins: [react(),tagger()],
-  resolve: {
-    alias: {
-      '@': '/src',
-      '@components': '/src/components',
-      '@pages': '/src/pages',
-      '@assets': '/src/assets',
-      '@constants': '/src/constants',
-      '@styles': '/src/styles',
+    base: '/bigbets/', // 👈 important
+    build: {
+        outDir: "build",
     },
-  },
-  server: {
-    port: "4028",
-    host: "0.0.0.0",
-    strictPort: true,
-    allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
-  }
+    plugins: [react(), tagger()],
+    resolve: {
+        alias: {
+            '@': '/src',
+            '@components': '/src/components',
+            '@pages': '/src/pages',
+            '@assets': '/src/assets',
+            '@constants': '/src/constants',
+            '@styles': '/src/styles',
+        },
+    },
+    server: {
+        port: 4028,
+        host: "0.0.0.0",
+        strictPort: true,
+    }
 });
